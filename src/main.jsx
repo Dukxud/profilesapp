a04b547101fb837f2219789338b7dabcf79e4fb2
Nathan Blackthorn <dukxud+git@pm.me>
2025-08-28 22:55:49 -0400

    Login capability

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import './index.css';
+import App from './App.jsx';
+import outputs from '../amplify_outputs.json';
+Amplify.configure(outputs);
+
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
