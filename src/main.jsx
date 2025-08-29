941c98c2d4223f278f1d73a9c3e0918a457121e4
Nathan Blackthorn <dukxud+git@pm.me>
2025-08-28 23:02:12 -0400

    resovled redundant 'App' import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Amplify } from 'aws-amplify';
 import './index.css';
-import App from './App.jsx';
 import outputs from '../amplify_outputs.json';
 Amplify.configure(outputs);
 
