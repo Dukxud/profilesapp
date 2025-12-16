1522d533333c367b5ab8378e3cbad58b1d0e1e44
Nathan Blackthorn <dukxud+git@pm.me>
2025-12-16 14:57:39 -0500

    Discount Code

--- src/billing-lambda/index.js
+++ src/billing-lambda/index.js
@@ -64,6 +64,7 @@ exports.handler = async (event) => {
       ],
       payment_method_types: ['card'],
       customer_email: email || undefined,
+      allow_promotion_codes: true,
       success_url: `${frontend}/billing?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${frontend}/billing?canceled=1`,
     });
